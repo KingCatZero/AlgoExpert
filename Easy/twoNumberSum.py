0c7f4c03bda91e9e5b39696fa99b537291b3b021
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2021-12-31 14:08:13 +0000

    Update twoNumberSum.py

--- Easy/twoNumberSum.py
+++ Easy/twoNumberSum.py
@@ -1,1 +1,12 @@
-
+def twoNumberSum(array, targetSum):
+    lookup = set()
+    
+	for a in array:		
+		b = targetSum - a
+        
+		if b in lookup:
+			return [b, a]
+		else:
+			lookup.add(a)
+            
+	return []
