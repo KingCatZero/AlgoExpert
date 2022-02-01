bd6f4b6327142425a8f725252caab8c094ec7e05
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2022-02-01 11:21:23 +0000

    Create numberOfWaysToMakeChange.py

---
+++ Medium/numberOfWaysToMakeChange.py
@@ -0,0 +1,9 @@
+def numberOfWaysToMakeChange(n, denoms):
+    numberOfWays = [1] + ([0] * n)
+	
+	for d in denoms:
+		print(numberOfWays)
+		for i in range(d, n + 1):
+			numberOfWays[i] += numberOfWays[i - d]
+			
+	return numberOfWays[-1]
