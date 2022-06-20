d881edd145ea75d70609df57628b3de8ba21d5ae
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2022-06-20 21:57:51 +0100

    Update maximizeExpression.py

--- Hard/maximizeExpression.py
+++ Hard/maximizeExpression.py
@@ -9,7 +9,7 @@ def maximizeExpression(array):
     signs = [1, -1, 1, -1]
     
     for i in range(1, n + 1):
-        for j in range(i, len(array) + i - 3):
+        for j in range(i, len(array) + i - n + 1):
             maxTerms[i][j] = max(maxTerms[i][j - 1], maxTerms[i - 1][j - 1] + (array[j - 1] * signs[i - 1]))
         
     return maxTerms[-1][-1]
