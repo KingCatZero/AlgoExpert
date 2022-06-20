af56fe955ad08e81e199236d70bfbe69950090c6
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2022-06-20 21:56:16 +0100

    Create maximizeExpression.py

---
+++ Hard/maximizeExpression.py
@@ -0,0 +1,15 @@
+def maximizeExpression(array):
+    n = 4
+    
+    if len(array) < n:
+        return 0
+    
+    maxTerms = [([float('-inf')] * (len(array) + 1)) for i in range(n)]
+    maxTerms.insert(0, [0] * (len(array) + 1))
+    signs = [1, -1, 1, -1]
+    
+    for i in range(1, n + 1):
+        for j in range(i, len(array) + i - 3):
+            maxTerms[i][j] = max(maxTerms[i][j - 1], maxTerms[i - 1][j - 1] + (array[j - 1] * signs[i - 1]))
+        
+    return maxTerms[-1][-1]
